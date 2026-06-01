d2f79d7fa33948b562b285a2328a7a61541fdfb4
3narinder <narinderd9@gmail.com>
2026-06-01 15:31:17 +0530

    system role, agent behavoiur and character, prompt messaging structure

--- invoke/app.js
+++ invoke/app.js
@@ -6,13 +6,31 @@ const groq = new Groq({ apikey: process.env.GROQ_API_KEY });
 
 async function main() {
   const response = await generateGroqResponse();
-  console.log(response);
+  console.log(response.choices[0].message);
 }
 
 export const generateGroqResponse = async () => {
   return groq.chat.completions.create({
     model: "llama-3.3-70b-versatile",
-    messages: [{ role: "user", content: "hi" }],
+    messages: [
+      {
+        role: "system",
+        content:
+          "you are a fitness coach Super. behave as if you are a helpful assistant. Check if user workout is good enough or not.Correct it in brief and simple way.",
+      },
+      {
+        role: "user",
+        content: `Monday:push, Tuesday:pull, Wednesday:legs rest, Thursday:push, Friday:pull, Saturday:legs rest, Sunday:rest.
+          
+          issue:
+
+          fixes:
+
+          routine:
+
+          `,
+      },
+    ],
   });
 };
 
