537fbab6ee968bde7ef34f3871bdcfeeed0c9697
3narinder <narinderd9@gmail.com>
2026-06-02 16:50:11 +0530

    Structure output : json_object method

--- invoke/app.js
+++ invoke/app.js
@@ -1,5 +1,4 @@
 import "./config.js";
-
 import Groq from "groq-sdk";
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
@@ -7,22 +6,21 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 export const generateGroqResponse = async () => {
   return groq.chat.completions.create({
     temperature: 0,
-    frequency_penalty: 0,
     model: "llama-3.3-70b-versatile",
+    response_format: { type: "json_object" },
     messages: [
       {
         role: "system",
-        content: "You are a creative writer.",
+        content:
+          "You are a data extractor. Parse the text and respond ONLY in JSON format.",
       },
       {
         role: "user",
-        content:
-          "Give me a unique, 1-sentence sci-fi story idea about a time traveler.",
+        content: "John Doe is 30 years old and lives in New York.",
       },
     ],
   });
 };
-
 async function main() {
   const resp1 = await generateGroqResponse();
   console.log(resp1.choices[0].message.content);
